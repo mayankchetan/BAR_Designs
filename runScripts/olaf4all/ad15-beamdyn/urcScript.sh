c02092d3a3f65ffc66d989b06799989d4db652e6
Mayank Chetan <mayankchetan@gmail.com>
2022-05-24 23:02:10 -0700

    new OF runs

---
+++ runScripts/olaf4all/ad15-beamdyn/urcScript.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=bar
+#SBATCH --time=2-00:00:00
+#SBATCH --job-name=urc_ad15_bd_bar
+#SBATCH --nodes=1             # This should be nC/36 (36 cores on eagle)
+#SBATCH --ntasks-per-node=36
+#SBATCH --mail-user mchetan@nrel.gov
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --output=logs/%j.urc_ad15_bd_bar.log
+#### ####SBATCH --partition=debug
+
+
+nDV=2 # Number of design variables (x2 for central difference)
+nOF=1  # Number of openfast runs per finite-difference evaluation
+nC=$((nDV + nDV * nOF)) # Number of cores needed. Make sure to request an appropriate number of nodes = N / 36
+nC=36
+
+module purge
+module load conda cmake
+module load comp-intel intel-mpi mkl
+module unload gcc
+module list
+
+source activate /home/mchetan/.conda-envs/weis-env-olaf-04may22
+which python
+
+python runBarURC.py
+# python weis_driverumaine_semi.py
--- runScripts/olaf4all/olaf-beamdyn/runBarUSC_ed_vizTest.py
+++ runScripts/olaf4all/olaf-beamdyn/runBarUSC_ed_vizTest.py
@@ -13,35 +13,22 @@ from wisdem.commonse.mpi_tools  import MPI
 import numpy as np
 import os, platform
 
-try:
-    if os.environ['NREL_CLUSTER'] == 'eagle':
-        eagleFlag = True
-    else:
-        eagleFlag = False
-except KeyError:
-    eagleFlag = False
-
 # Paths calling the standard modules of WEIS
 fastBatch = runFAST_pywrapper_batch()
 run_dir1                    = os.path.join(os.path.dirname(weis.__file__) + os.sep + '..' + os.sep)
 run_dir2                    = os.path.dirname( os.path.realpath(__file__) ) + os.sep
-
-if eagleFlag:
-    save_dir                = '/projects/bar/mchetan/projects/downwind/olaf-bd'
-else:
-    save_dir                = './outputs/olaf-ed-test'
-
-fastBatch.FAST_directory    = os.path.join(run_dir2,'../../../BAR_DRC/' ,'of-olaf') # Path to fst directory files
-fastBatch.FAST_InputFile    = 'BAR_DRC_out_0.fst'   # FAST input file (ext=.fst)
-fastBatch.FAST_runDirectory = os.path.join(save_dir + 'outputs' + os.sep + 'drc')
+save_dir                    = './outputs/olaf-ed-test'#'/projects/bar/mchetan/projects/downwind/olaf-bd'
+fastBatch.FAST_directory    = os.path.join(run_dir2,'../../../BAR_USC/' ,'of-olaf') # Path to fst directory files
+fastBatch.FAST_InputFile    = 'BAR_USC_out_0.fst'   # FAST input file (ext=.fst)
+fastBatch.FAST_runDirectory = os.path.join(save_dir + 'outputs' + os.sep + 'usc')
 fastBatch.debug_level       = 2
 
 # User settings
-n_cores     = 36     # Number of available cores
-TMax        = 720.    # Length of wind grids and OpenFAST simulations, suggested 720 s
-runTime     = 600.    # Suggested at 600. s
-cut_in      = 3.    # Cut in wind speed
-cut_out     = 25.   # Cut out wind speed
+n_cores     = 1     # Number of available cores
+TMax        = 150.    # Length of wind grids and OpenFAST simulations, suggested 720 s
+runTime     = 50.    # Suggested at 600. s
+cut_in      = 8.    # Cut in wind speed
+cut_out     = 9.   # Cut out wind speed
 n_ws        = cut_out - cut_in + 1    # Number of wind speed bins
 wind_speeds = np.linspace(int(cut_in), int(cut_out), int(n_ws)) # Wind speeds to run OpenFAST at
 Ttrans      = max([0., TMax - 60.])  # Start of the transient for DLC with a transient, e.g. DLC 1.4
@@ -49,13 +36,12 @@ TStart      = max([0., TMax - runTime]) # Start of the recording of the channels
 
 # Initial conditions for ElastoDyn
 u_ref       = np.arange(3.,26.) # Wind speed vector to specify the initial conditions
-pitch_ref   = [ 0.    ,  0.    ,  0.    ,  0.    ,  0.    ,  0.8669,  2.81  ,
-        6.178 ,  8.604 , 10.4   , 12.27  , 13.84  , 15.25  , 16.81  ,
-       18.06  , 19.34  , 20.71  , 21.79  , 23.01  , 24.27  , 25.29  ,
-       26.44  , 27.62  ] # Pitch values in deg
-omega_ref   = [2.879, 3.775, 4.685, 5.591, 6.49 , 7.309, 7.575, 7.587, 7.56 ,
-       7.584, 7.582, 7.532, 7.606, 7.578, 7.542, 7.618, 7.549, 7.548,
-       7.648, 7.539, 7.526, 7.664, 7.547] # Rotor speeds in rpm
+pitch_ref   = [ 0.   ,  0.   ,  0.   ,  0.   ,  0.   ,  1.317,  3.109,  4.443,
+        9.287, 10.05 , 10.64 , 12.16 , 14.39 , 16.08 , 17.08 , 18.31 ,
+       19.52 , 20.67 , 21.78 , 22.9  , 24.   , 25.07 , 26.15 ] # Pitch values in deg
+omega_ref   = [3.441, 3.847, 4.746, 5.699, 6.649, 7.505, 7.57 , 7.604, 7.576,
+       7.494, 7.557, 7.554, 7.497, 7.564, 7.57 , 7.579, 7.582, 7.563,
+       7.584, 7.586, 7.561, 7.585, 7.593] # Rotor speeds in rpm
 pitch_init = np.interp(wind_speeds, u_ref, pitch_ref)
 omega_init = np.interp(wind_speeds, u_ref, omega_ref)
 
@@ -64,7 +50,7 @@ adTimeSteps =  5 * 60 / 360 / np.array(omega_init) # Time for 5 degree rotation
 # Settings passed to OpenFAST
 case_inputs = {}
 case_inputs[("Fst","TMax")]             = {'vals':[TMax], 'group':0}
-case_inputs[("Fst","DT")]               = {'vals':[0.0002], 'group':0}
+case_inputs[("Fst","DT")]               = {'vals':[0.002], 'group':0}
 case_inputs[("Fst","DT_Out")]           = {'vals':[0.1], 'group':0}
 case_inputs[("ServoDyn","DLL_DT")]      = {'vals':[0.01], 'group':0}
 case_inputs[("Fst","CompInflow")]       = {'vals':[1], 'group':0}
@@ -72,20 +58,19 @@ case_inputs[("Fst","CompInflow")]       = {'vals':[1], 'group':0}
 case_inputs[("Fst","SttsTime")]       = {'vals':[1], 'group':0}
 case_inputs[("Fst","TStart")]       = {'vals':[0], 'group':0}
 
-# Below for BeamDyn
-case_inputs[("Fst","CompElast")]       = {'vals':[2], 'group':0}
+case_inputs[("Fst","CompElast")]       = {'vals':[1], 'group':0}
 
 case_inputs[("Fst","CompServo")]        = {'vals':[1], 'group':0}
 case_inputs[("Fst","OutFileFmt")]       = {'vals':[1], 'group':0}
 case_inputs[("ElastoDyn","GenDOF")]     = {'vals':['True'], 'group':0}
 case_inputs[("ServoDyn","PCMode")]      = {'vals':[5], 'group':0}
 case_inputs[("ServoDyn","VSContrl")]    = {'vals':[5], 'group':0}
-
-# Below for OLAF
 case_inputs[("AeroDyn15","WakeMod")]    = {'vals':[3], 'group':0}
 case_inputs[("AeroDyn15","AFAeroMod")]    = {'vals':[1], 'group':0}
 
 case_inputs[("AeroDyn15","DTAero")]    = {'vals': adTimeSteps, 'group':1}
+
+case_inputs[("AeroDyn15","OLAF","WrVTk")] = {'vals':[1], 'group':0}
 
 case_inputs[("InflowWind","WindType")]  = {'vals':[1], 'group':0}
 case_inputs[("InflowWind","HWindSpeed")]= {'vals': wind_speeds, 'group': 1}
@@ -117,7 +102,7 @@ else:
 case_inputs[("ServoDyn","DLL_FileName")] = {'vals':[path2dll], 'group':0}
 
 # Generate the matrix of cases
-case_list, case_name_list = CaseGen_General(case_inputs, dir_matrix=fastBatch.FAST_runDirectory, namebase='bar-drc')
+case_list, case_name_list = CaseGen_General(case_inputs, dir_matrix=fastBatch.FAST_runDirectory, namebase='bar-usc-8')
 
 fastBatch.case_list = case_list
 fastBatch.case_name_list = case_name_list
