5f960054d436e6a6dda9debf881cf0ece6c4651f
Mayank Chetan <mchetan@nrel.gov>
2022-06-14 16:22:32 -0600

    Stiffer torsion for BD

--- runScripts/olaf4all/ad15-beamdyn/runAllArgs.sh
+++ runScripts/olaf4all/ad15-beamdyn/runAllArgs.sh
@@ -26,56 +26,56 @@
 
 
 
-# TURB = 'URC'
-
-# echo "$TURB Running from 3 to 6 m/s for 4 cores"
-# sbatch urcArgBasedScript.sh 3 6 4
-# sleep 5
-
-# echo "$TURB Running from 7 to 10 m/s for 4 cores"
-# sbatch urcArgBasedScript.sh 7 10 4
-# sleep 5
-
-# echo "$TURB Running from 11 to 14 m/s for 4 cores"
-# sbatch urcArgBasedScript.sh 11 14 4
-# sleep 5
-
-# echo "$TURB Running from 15 to 18 m/s for 4 cores"
-# sbatch urcArgBasedScript.sh 15 18 4
-# sleep 5
-
-# echo "$TURB Running from 19 to 22 m/s for 4 cores"
-# sbatch urcArgBasedScript.sh 19 22 4
-# sleep 5
-
-# echo "$TURB Running from 23 to 25 m/s for 4 cores"
-# sbatch urcArgBasedScript.sh 23 25 4
-# sleep 5
-
-
-
-TURB = 'DRC'
+TURB='URC'
 
 echo "$TURB Running from 3 to 6 m/s for 4 cores"
-sbatch drcArgBasedScript.sh 3 6 4
+sbatch urcArgBasedScript_stiffTorr.sh 3 6 4
 sleep 5
 
 echo "$TURB Running from 7 to 10 m/s for 4 cores"
-sbatch drcArgBasedScript.sh 7 10 4
+sbatch urcArgBasedScript_stiffTorr.sh 7 10 4
 sleep 5
 
 echo "$TURB Running from 11 to 14 m/s for 4 cores"
-sbatch drcArgBasedScript.sh 11 14 4
+sbatch urcArgBasedScript_stiffTorr.sh 11 14 4
 sleep 5
 
 echo "$TURB Running from 15 to 18 m/s for 4 cores"
-sbatch drcArgBasedScript.sh 15 18 4
+sbatch urcArgBasedScript_stiffTorr.sh 15 18 4
 sleep 5
 
 echo "$TURB Running from 19 to 22 m/s for 4 cores"
-sbatch drcArgBasedScript.sh 19 22 4
+sbatch urcArgBasedScript_stiffTorr.sh 19 22 4
 sleep 5
 
 echo "$TURB Running from 23 to 25 m/s for 4 cores"
-sbatch drcArgBasedScript.sh 23 25 4
+sbatch urcArgBasedScript_stiffTorr.sh 23 25 4
 sleep 5
+
+
+
+# TURB = 'DRC'
+
+# echo "$TURB Running from 3 to 6 m/s for 4 cores"
+# sbatch drcArgBasedScript.sh 3 6 4
+# sleep 5
+
+# echo "$TURB Running from 7 to 10 m/s for 4 cores"
+# sbatch drcArgBasedScript.sh 7 10 4
+# sleep 5
+
+# echo "$TURB Running from 11 to 14 m/s for 4 cores"
+# sbatch drcArgBasedScript.sh 11 14 4
+# sleep 5
+
+# echo "$TURB Running from 15 to 18 m/s for 4 cores"
+# sbatch drcArgBasedScript.sh 15 18 4
+# sleep 5
+
+# echo "$TURB Running from 19 to 22 m/s for 4 cores"
+# sbatch drcArgBasedScript.sh 19 22 4
+# sleep 5
+
+# echo "$TURB Running from 23 to 25 m/s for 4 cores"
+# sbatch drcArgBasedScript.sh 23 25 4
+# sleep 5
---
+++ runScripts/olaf4all/ad15-beamdyn/urcArgBasedScript_stiffTorr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --account=bar
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=urc_arg_ad15
+#SBATCH --nodes=1             # This should be nC/36 (36 cores on eagle)
+#SBATCH --ntasks-per-node=36
+#SBATCH --mail-user mchetan@nrel.gov
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --output=logs/%j.urc_args.log
+#### ####SBATCH --partition=debug
+
+
+nDV=2 # Number of design variables (x2 for central difference)
+nOF=1  # Number of openfast runs per finite-difference evaluation
+nC=$((nDV + nDV * nOF)) # Number of cores needed. Make sure to request an appropriate number of nodes = N / 36
+nC=36
+
+module purge
+module load conda cmake
+module load comp-intel intel-mpi mkl
+module unload gcc
+module list
+
+source activate /home/mchetan/.conda-envs/weis-env-olaf-04may22
+which python
+
+echo " "
+echo "******* RUNNING URC ********"
+echo "Start WS = $1; End WS = $2; Cores = $3"
+echo "***************"
+echo " "
+
+python runBarURC_args_stiffTorr.py --startW $1 --endW $2 --numCores $3
